51dfc4f7806f9f59c6f70fb86901f501c93e5b1c
Eidur <ek040803@gmail.com>
2023-04-10 18:45:54 +0000

    bætti við stylesheet og bjó til dropdown-view.fxml

--- src/main/java/is/hi/hopur/lokaverkefni/CheckboxingController.java
+++ src/main/java/is/hi/hopur/lokaverkefni/CheckboxingController.java
@@ -3,7 +3,7 @@ package is.hi.hopur.lokaverkefni;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class CheckboxingController {
     @FXML
     private Label welcomeText;
 
