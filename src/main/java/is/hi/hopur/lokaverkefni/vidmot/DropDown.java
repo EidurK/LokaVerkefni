97f3b6c9bd36b50e7c8376dc601b4720a7996e14
Eidur <ek040803@gmail.com>
2023-04-13 14:55:55 +0000

    Kláraði grunninn að verkefninu

--- src/main/java/is/hi/hopur/lokaverkefni/vidmot/DropDown.java
+++ src/main/java/is/hi/hopur/lokaverkefni/vidmot/DropDown.java
@@ -1,4 +1,8 @@
 package is.hi.hopur.lokaverkefni.vidmot;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ProgressBar;
@@ -24,6 +28,7 @@ public class DropDown extends AnchorPane  {
     public DropDown(){
         FXML_Lestur.lesa(this, "dropdown-view.fxml");
         TaskGeymsla taskGeymsla = new TaskGeymsla();
+
         fxComboBox.setSkin(new ComboBoxListViewSkin<AnchorPane>(fxComboBox){
             @Override
             public void hide() {
@@ -31,6 +36,6 @@ public class DropDown extends AnchorPane  {
             }
         });
         fxComboBox.setItems(taskGeymsla.getItemObservableList());
-        fxProgressBar.progressProperty().bind(taskGeymsla.checkedPercentageProperty());
+        fxProgressBar.progressProperty().bind(taskGeymsla.getPercentage());
     }
 }
--- src/main/java/is/hi/hopur/lokaverkefni/vinnsla/TaskGeymsla.java
+++ src/main/java/is/hi/hopur/lokaverkefni/vinnsla/TaskGeymsla.java
@@ -2,9 +2,14 @@ package is.hi.hopur.lokaverkefni.vinnsla;
 
 import is.hi.hopur.lokaverkefni.vidmot.AddTask;
 import is.hi.hopur.lokaverkefni.vidmot.Task;
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.beans.value.ObservableValueBase;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.scene.control.CheckBox;
 import javafx.scene.layout.AnchorPane;
 
@@ -14,17 +19,27 @@ public class TaskGeymsla {
     private ObservableList<AnchorPane> itemObservableList = FXCollections.observableArrayList();
     private ObservableList<Task> taskObservableList = FXCollections.observableArrayList();
 
-    public double getCheckedPercentage() {
-        return checkedPercentage.get();
+    public Double getChecked() {
+        return checked.getValue();
     }
 
-    public SimpleDoubleProperty checkedPercentageProperty() {
-        return checkedPercentage;
+
+    private ObservableValue<Double> checked = new ObservableValueBase<>() {
+        @Override
+        public Double getValue() {
+            if(taskObservableList.isEmpty()){
+                return 0.0;
+            }
+            return Bindings.size((new FilteredList<>(taskObservableList, task -> task.getFxCheckBox().isSelected()))).doubleValue()/Bindings.size(taskObservableList).doubleValue();
+        }
+    };
+
+    private SimpleDoubleProperty percentage = new SimpleDoubleProperty();
+    public SimpleDoubleProperty getPercentage(){
+        return percentage;
     }
 
-    private SimpleDoubleProperty checkedPercentage;
     public TaskGeymsla(){
-        checkedPercentage = new SimpleDoubleProperty();
         AddTask addTaskButton = new AddTask();
         addTaskButton.getFxAddItemButton().setOnAction(actionEvent -> {
             addTask(new Task());
@@ -34,8 +49,13 @@ public class TaskGeymsla {
     public void addItem(AnchorPane t){
         itemObservableList.add(t);
         if(t.getClass().equals(Task.class)){
-            taskObservableList.add((Task) t);
+            Task task = (Task) t;
+            task.getFxCheckBox().selectedProperty().addListener(c ->{
+                percentage.set(getChecked());
+            });
+            taskObservableList.add(task);
         }
+        percentage.set(getChecked());
     }
     public void addTask(Task t){
         addItem(t);
